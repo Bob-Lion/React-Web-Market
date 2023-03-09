e7cd822b3d9467db6c019b87a8460f461b081417
djswns7 <duftlagltkfwk@gmail.com>
2023-03-10 02:01:36 +0900

    setting: vite 기본 플러그인 세팅 및 eslint, prettier 추가

---
+++ .eslintrc.cjs
@@ -0,0 +1,66 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "import", "react-hooks", "jsx-a11y"],
+  rules: {
+    "jsx-a11y/anchor-has-content": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "no-console": "warn",
+    "react/prop-types": "off",
+    "react/button-has-type": "warn",
+    "react/self-closing-comp": [
+      "warn",
+      {
+        component: true,
+        html: false,
+      },
+    ],
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        shorthandFirst: true,
+        callbacksLast: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        multiline: "last",
+      },
+    ],
+  },
+}
