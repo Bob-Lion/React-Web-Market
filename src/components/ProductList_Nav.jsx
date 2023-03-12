806c3e556dc57941ac4fba130b83c46dae9f5ed4
leejungho <ljhmoa@gmail.com>
2023-03-12 11:14:17 +0900

    feat: ProductList 구조 설계 #14
    - ProductList 생성
    - ProductList-Nav 생성
    - Product-Nav-Detail 생성

---
+++ src/components/ProductList_Nav.jsx
@@ -0,0 +1,5 @@
+import styled from './ProductList_Nav.module.scss';
+
+export function ProductList_Nav() {
+  return <div>프로덕트 리스트 네비게이션</div>;
+}
---
+++ src/pages/ProductList/ProductList.jsx
@@ -0,0 +1,11 @@
+// product-list 페이지
+
+import { ProductList_Nav } from '../../components/ProductList_Nav';
+
+export function ProductList() {
+  return (
+    <>
+      <ProductList_Nav />
+    </>
+  );
+}
