0e7094f280bd812c38c843c8ca8540b9c1ba4312
leejungho <ljhmoa@gmail.com>
2023-03-13 15:06:05 +0900

    feat: Accordion Set 스타일링 #14

--- src/components/ProductListNav/AccordionSet.jsx
+++ src/components/ProductListNav/AccordionSet.jsx
@@ -1,20 +1,20 @@
-import arrow from '@/assets/ProductListImage/Arrow.svg';
+import arrow from '@/../public/ProductListImage/Arrow.svg';
 import { AccordionList } from './AccordionList';
-import leftarrow from '@/assets/ProductListImage/Arrow.svg';
+import leftarrow from '@/../public/ProductListImage/Arrow.svg';
 import styles from './ProductListNav.module.scss';
 // 아코디언 목록 한 세트
 export function AccordionSet({ data, listName }) {
   return (
-    <div>
-      <button>
-        <span>{listName}</span>
+    <div className={styles.accordionSet}>
+      <button className={styles.accordionSetButton}>
+        <span className={styles.accordionSetButtonText}>{listName}</span>
         <span>
           <img src={arrow} alt="해당 아코디언 목록 펼치고 닫는 화살표" />
         </span>
       </button>
-      <ul>
-        {data.map((list) => {
-          return <AccordionList key={list[0]} name={list[0]} count={list[1]} />;
+      <ul className={styles.accordionSetBody}>
+        {data.map((item) => {
+          return <AccordionList key={item[0]} name={item[0]} count={item[1]} />;
         })}
       </ul>
       <button>
