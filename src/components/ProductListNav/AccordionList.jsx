c05096443ad1aef7cf42cafe7eb0da05fb1fa776
leejungho <ljhmoa@gmail.com>
2023-03-13 16:18:48 +0900

    feat: Accordion List Item 스타일링 완성 #14

--- src/components/ProductListNav/AccordionList.jsx
+++ src/components/ProductListNav/AccordionList.jsx
@@ -1,16 +1,17 @@
 import click_check_on from '@/../public/ProductListImage/Check_on.svg';
 import click_check_off from '@/../public/ProductListImage/Check_off.svg';
+import styles from './ProductListNav.module.scss';
 
 //아코디언 목록의 리스트
 export function AccordionList({ name, count }) {
   return (
-    <li>
-      <a className={`.willRouter`} href="/">
-        <button>
+    <li className={styles.accordionList}>
+      <a className={`.willRouter ${styles.accordionListItem}`} href="/">
+        <button className={styles.accordionListItemcheckButton}>
           <img src={click_check_off} alt="해당 리스트 체크하는 버튼"></img>
         </button>
-        <span>{name}</span>
-        <span>{count}</span>
+        <span className={styles.accordionListItemName}>{name}</span>
+        <span className={styles.accordionListItemCount}>{count}</span>
       </a>
     </li>
   );
