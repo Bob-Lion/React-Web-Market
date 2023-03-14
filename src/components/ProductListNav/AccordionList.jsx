48ddd7258443a4428105e80d31e60dce521f8f17
leejungho <ljhmoa@gmail.com>
2023-03-14 11:19:54 +0900

    feat: 아코디언 리스트 마우스 오버시 글자색 변환 #14

--- src/components/ProductListNav/AccordionList.jsx
+++ src/components/ProductListNav/AccordionList.jsx
@@ -1,16 +1,32 @@
 import click_check_on from '@/../public/ProductListImage/Check_on.svg';
 import click_check_off from '@/../public/ProductListImage/Check_off.svg';
 import styles from './ProductListNav.module.scss';
+import { useState, useRef } from 'react';
 
 //아코디언 목록의 리스트
 export function AccordionList({ name, count }) {
+  // const [over, setOver] = useState(true);
+  const hoverSpan = useRef();
+  const handleEnter = () => {
+    hoverSpan.current.style.color = 'rgb(209, 122, 1)';
+  };
+  const handleLeave = () => {
+    hoverSpan.current.style.color = 'rgb(51, 51, 51)';
+  };
   return (
     <li className={styles.accordionList}>
-      <a className={`.willRouter ${styles.accordionListItem}`} href="/">
+      <a className={`.willRouter ${styles.accordionListItem}`} href="./">
         <button className={styles.accordionListItemcheckButton}>
           <img src={click_check_off} alt="해당 리스트 체크하는 버튼"></img>
         </button>
-        <span className={styles.accordionListItemName}>{name}</span>
+        <span
+          className={styles.accordionListItemName}
+          ref={hoverSpan}
+          onMouseEnter={handleEnter}
+          onMouseLeave={handleLeave}
+        >
+          {name}
+        </span>
         <span className={styles.accordionListItemCount}>{count}</span>
       </a>
     </li>
