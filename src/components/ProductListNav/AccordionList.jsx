fd74adab9443736794f1fbfd7dcd82c4e37889e6
leejungho <ljhmoa@gmail.com>
2023-03-16 03:03:40 +0900

    feat: 필터 목록 리스트 클릭에 따른 클릭 항목 상태 관리 #35

--- src/components/ProductListNav/AccordionList.jsx
+++ src/components/ProductListNav/AccordionList.jsx
@@ -5,7 +5,7 @@ import { useState, useRef } from 'react';
 
 //아코디언 목록의 리스트
 export function AccordionList({ name, count, selectData }) {
-  const [btnToggle, setBtnToggle] = useState(true);
+  const [btnToggle, setBtnToggle] = useState(false);
   const hoverSpan = useRef();
   const handleEnter = () => {
     hoverSpan.current.style.color = 'rgb(209, 122, 1)';
@@ -17,7 +17,14 @@ export function AccordionList({ name, count, selectData }) {
   const clickProduct = [];
 
   const handleClickCheck = () => {
-    selectData.current.push(name);
+    // 체크 버튼이 활성화 될때 값이 배열에 없으면 추가하고 체크버튼이 비활성화 될때 해당 값을 삭제해 준다.
+    if (!btnToggle && !selectData.current.includes(name)) {
+      selectData.current.push(name);
+    } else if (btnToggle && selectData.current.includes(name)) {
+      const index = selectData.current.indexOf(name);
+      selectData.current.splice(index, 1);
+    }
+
     console.log(selectData.current);
     // console.log(name);
 
@@ -47,7 +54,7 @@ export function AccordionList({ name, count, selectData }) {
         <button className={styles.accordionListItemcheckButton} type="button">
           <img
             alt="해당 리스트 체크하는 버튼"
-            src={btnToggle ? click_check_off : click_check_on}
+            src={btnToggle ? click_check_on : click_check_off}
           ></img>
         </button>
         <span
