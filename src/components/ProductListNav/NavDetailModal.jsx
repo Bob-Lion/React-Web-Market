fdf3f8f4fd17d1b3bd57e38d234167a9add7f540
leejungho <ljhmoa@gmail.com>
2023-03-17 02:40:10 +0900

    feat: 더보기 모달창 기본 마크업 (#15)

--- src/components/ProductListNav/NavDetailModal.jsx
+++ src/components/ProductListNav/NavDetailModal.jsx
@@ -5,9 +5,29 @@ export function NaveDetailModal({ data, listName }) {
         <strong>{listName}</strong>
         {listName === '브랜드' ? (
           <ul>
-            <li></li>
+            <li>
+              <button type="button">가나다순</button>
+            </li>
+            <li>
+              <button type="button">상품 많은순</button>
+            </li>
           </ul>
         ) : null}
+        <button type="button">
+          <img src="" alt="" />
+        </button>
+      </div>
+      <ul>
+        <li></li>
+      </ul>
+      <div>
+        <button type="button">
+          <img src="" alt="" />
+          <span>초기화</span>
+        </button>
+        <button type="button">
+          <span>확인</span>
+        </button>
       </div>
     </div>
   );
