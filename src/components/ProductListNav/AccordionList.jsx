200b774eef78ad277a6f6c8662762c62fc4e2f40
JungHo LEE <66310772+wjdgh4058@users.noreply.github.com>
2023-03-17 02:26:39 +0900

    [Feat] 리스트 선택 초기화 구현 (#38)
    * feat: 필터 목록 리스트 클릭에 따른 클릭 항목 상태 관리 #35
    
    * feat: 선택 데이터 체크버튼 상태관리 완료 (#35)
    
    * faat: 초기화 클릭시 true 반환 (#35)
    
    * feat: 데이터 useRef 에서 useState 로 변경하여 불러오기 #35
    
    * feat: 초기화 전역 상태 생성 (#35)
    
    * feat: 초기화 필터에 설정 (#35)
    
    * feat: 초기화 하기 상태 업데이트만 해주면 완성 (#35)
    
    ---------
    
    Co-authored-by: Park Wonjun <72201211+djswns7@users.noreply.github.com>

--- src/components/ProductListNav/AccordionList.jsx
+++ src/components/ProductListNav/AccordionList.jsx
@@ -1,41 +1,48 @@
 import click_check_on from '@/../public/ProductListImage/Check_on.svg';
 import click_check_off from '@/../public/ProductListImage/Check_off.svg';
 import styles from './ProductListNav.module.scss';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { productListCheckReset } from '../../@atom/accordion/productListCheckReset';
 
 //아코디언 목록의 리스트
-export function AccordionList({ name, count, selectData }) {
-  const [btnToggle, setBtnToggle] = useState(true);
+export function AccordionList({ name, count, selectData, setSelectData }) {
+  const [btnToggle, setBtnToggle] = useState(false);
   const hoverSpan = useRef();
+  const listCheckReset = useSetRecoilState(productListCheckReset);
+
   const handleEnter = () => {
     hoverSpan.current.style.color = 'rgb(209, 122, 1)';
   };
   const handleLeave = () => {
     hoverSpan.current.style.color = 'rgb(51, 51, 51)';
   };
-
-  const clickProduct = [];
-
   const handleClickCheck = () => {
-    selectData.current.push(name);
-    console.log(selectData.current);
-    // console.log(name);
+    // 체크 버튼이 활성화 될때 값이 배열에 없으면 추가하고 체크버튼이 비활성화 될때 해당 값을 삭제해 준다.
+    if (!btnToggle && !selectData.includes(name)) {
+      selectData.push(name);
+    } else if (btnToggle && selectData.includes(name)) {
+      const index = selectData.indexOf(name);
+      selectData.splice(index, 1);
+    }
+
+
+    console.log(selectData);
+
+    // 리셋 버튼 활성화 상태 설정
+    if (selectData.length > 0) {
+
+      listCheckReset(true);
+    } else {
+      listCheckReset(false);
+    }
 
     setBtnToggle(!btnToggle);
   };
-  // 체크버튼 클릭시, 브랜드 정렬방법 선택시 데이터 관리하는거 상태관리 다시 해야댐
-  /* const btnToggleMemo = useMemo(() => {
-    return btnToggle ? click_check_off : click_check_on;
-  }, [btnToggle]);
-
-  const handleClickCheck = () => {
-    // console.log(name);
-    setBtnToggle(!btnToggle);
-  };
-
-  useEffect(() => {
-    // return setBtnToggle(!btnToggle);
-  }, [btnToggleMemo]); */
+  // useEffect(() => {
+  //   return selectData.includes(name) ? click_check_on : click_check_off;
+  // }, [name, selectData]);
 
   return (
     <li className={styles.accordionList}>
@@ -47,12 +54,13 @@ export function AccordionList({ name, count, selectData }) {
         <button className={styles.accordionListItemcheckButton} type="button">
           <img
             alt="해당 리스트 체크하는 버튼"
-            src={btnToggle ? click_check_off : click_check_on}
+            src={selectData.includes(name) ? click_check_on : click_check_off}
+            // src={check}
           ></img>
         </button>
         <span
-          className={styles.accordionListItemName}
           ref={hoverSpan}
+          className={styles.accordionListItemName}
           onMouseEnter={handleEnter}
           onMouseLeave={handleLeave}
         >
