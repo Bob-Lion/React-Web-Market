eba497f39878f2fbf62ae58744fa2244014d2308
leejungho <ljhmoa@gmail.com>
2023-03-22 02:13:29 +0900

    feat: 냉장,냉동,상온 분류 함수 작성 및 적용 (#41)

--- src/components/AddCartAccordion/AddCartAccordion.jsx
+++ src/components/AddCartAccordion/AddCartAccordion.jsx
@@ -1,13 +1,23 @@
+import { storingWaySort } from '@/utils/storingWay/storingWaySort';
 import { AddCartSet } from './AddCartSet';
 import { ProductSelectCtrl } from './ProductSelectCtrl';
 
-export function AddCartAccordion() {
+export function AddCartAccordion({ data }) {
+  // 냉장 식품 선별
+  const refrigerated = storingWaySort(data, '냉장');
+  // 냉동 식품 선별
+  const frozen = storingWaySort(data, '냉동');
+  // 상온 식품 선별
+  const roomTemperature = storingWaySort(data, '냉장');
+
   return (
     <div>
       <ProductSelectCtrl />
-      <AddCartSet />
-      <AddCartSet />
-      <AddCartSet />
+      {refrigerated.length > 0 ? <AddCartSet data={refrigerated} /> : null}
+      {frozen.length > 0 ? <AddCartSet data={frozen} /> : null}
+      {roomTemperature.length > 0 ? (
+        <AddCartSet data={roomTemperature} />
+      ) : null}
       <ProductSelectCtrl />
     </div>
   );
