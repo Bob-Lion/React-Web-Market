843d42065d9bdfb190fa62d062aa5bd50596123b
JungHo LEE <66310772+wjdgh4058@users.noreply.github.com>
2023-03-27 02:02:34 +0900

    [Feat] 장바구니 페이지 전체선택 제외 상태관리 완료 (#69)
    * feat: 장바구니 상품 선택에 따른 수량 변화 완성 (#62)
    
    * feat: 장바구니 로컬데이터로 연동 (#62)
    
    * feat: 장바구니 페이지 전체선택 상태관리 보완 필요 (#62)
    
    ---------
    
    Co-authored-by: Park Wonjun <72201211+djswns7@users.noreply.github.com>

--- src/components/CartPageAccordion/CartPageAccordion.jsx
+++ src/components/CartPageAccordion/CartPageAccordion.jsx
@@ -7,16 +7,24 @@ import frozenImg from '@/../public/icons/web-icons/Frozen.svg';
 import normalImg from '@/../public/icons/web-icons/Normal.svg';
 import { useRecoilState } from 'recoil';
 import { cartTotalSeletState } from '@/@atom/cartPage/cartTotalSeletState';
-// import styles from './CartPageAccordion.module.scss';
+import styles from './CartPageAccordion.module.scss';
 
 export function CartPageAccordion({ data }) {
   // console.log(data);
   // 냉장 식품 선별
-  const refrigerated = storingWaySort(data, '냉장');
+  let refrigerated = [];
   // 냉동 식품 선별
-  const frozen = storingWaySort(data, '냉동');
+  let frozen = [];
   // 상온 식품 선별
-  const roomTemperature = storingWaySort(data, '상온');
+  let roomTemperature = [];
+  if (data) {
+    // 냉장 식품 선별
+    refrigerated = storingWaySort(data, '냉장');
+    // 냉동 식품 선별
+    frozen = storingWaySort(data, '냉동');
+    // 상온 식품 선별
+    roomTemperature = storingWaySort(data, '상온');
+  }
 
   // 선택된 상품 개수 관리
   // const [checkTotalCount, setCheckTotalCount] =
@@ -25,40 +33,60 @@ export function CartPageAccordion({ data }) {
   //   setCheckTotalCount(data.length);
   // }, data);
   console.log(frozen);
+  if (data) {
+    return (
+      <div>
+        <ProductSelectCtrl
+          cartData={data}
+          // checkCount={checkTotalCount}
+          // setCheckTotalCount={setCheckTotalCount}
+        />
+        {refrigerated.length > 0 ? (
+          <CartPageSet
+            data={refrigerated}
+            productType={'냉장'}
+            storingWayImg={refrigeratedImg}
+          />
+        ) : null}
+        {frozen.length > 0 ? (
+          <CartPageSet
+            data={frozen}
+            productType={'냉동'}
+            storingWayImg={frozenImg}
+          />
+        ) : null}
+        {roomTemperature.length > 0 ? (
+          <CartPageSet
+            data={roomTemperature}
+            productType={'상온'}
+            storingWayImg={normalImg}
+          />
+        ) : null}
+        <ProductSelectCtrl
+          cartData={data}
+          // checkCount={checkTotalCount}
+          // setCheckTotalCount={setCheckTotalCount}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ProductSelectCtrl
+          cartData={data}
+          // checkCount={checkTotalCount}
+          // setCheckTotalCount={setCheckTotalCount}
+        />
+        <div className={styles.nonAddCartProduct}>
+          장바구니에 담긴 상품이 없습니다.
+        </div>
 
-  return (
-    <div>
-      <ProductSelectCtrl
-        cartData={data}
-        // checkCount={checkTotalCount}
-        // setCheckTotalCount={setCheckTotalCount}
-      />
-      {refrigerated.length > 0 ? (
-        <CartPageSet
-          data={refrigerated}
-          productType={'냉장'}
-          storingWayImg={refrigeratedImg}
+        <ProductSelectCtrl
+          cartData={data}
+          // checkCount={checkTotalCount}
+          // setCheckTotalCount={setCheckTotalCount}
         />
-      ) : null}
-      {frozen.length > 0 ? (
-        <CartPageSet
-          data={frozen}
-          productType={'냉동'}
-          storingWayImg={frozenImg}
-        />
-      ) : null}
-      {roomTemperature.length > 0 ? (
-        <CartPageSet
-          data={roomTemperature}
-          productType={'상온'}
-          storingWayImg={normalImg}
-        />
-      ) : null}
-      <ProductSelectCtrl
-        cartData={data}
-        // checkCount={checkTotalCount}
-        // setCheckTotalCount={setCheckTotalCount}
-      />
-    </div>
-  );
+      </div>
+    );
+  }
 }
