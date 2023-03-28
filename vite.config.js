6cf5c01652c12ec23dd573f6d4bfa7afd6fcd9a4
JinYee Jeong <luvdo0112@daum.net>
2023-03-29 01:20:45 +0900

    deploy : exact수정, vite.config base 수정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-Web-Market/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
