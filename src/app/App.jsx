750dc6cb53070dd2d9e4af75b349fb5ebed8f058
JinYee Jeong <luvdo0112@daum.net>
2023-03-28 19:52:39 +0900

    fix : App.jsx 주석 처리 해제

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import RegisterPage from '@/pages/Register/RegisterPage';
 import HomePage from '@/pages/Home/HomePage';
-// import RegisterPage from '@/pages/Register/RegisterPage';
 import { CartPage } from '@/pages/cartPage/CartPage';
 import { ProductList } from '@/pages/ProductList/ProductList';
 import ProductDetailPage from '@/pages/ProductDetail/ProductDetailPage';
@@ -13,7 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route element={<HomePage />} path="/" />
-        {/* <Route element={<RegisterPage />} path="/register" /> */}
+        <Route element={<RegisterPage />} path="/register" />
         <Route element={<CartPage />} path="/cart" />
         <Route element={<LoginPage />} path="/logIn" />
         <Route element={<ProductList />} path="/productList" />
